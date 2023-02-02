f1d14e9126eb0f84d43dee3b1b03334d10724dc9
Evan Clemens <92829330+Eclemens86@users.noreply.github.com>
2023-02-02 15:57:07 -0600

    Add files via upload

---
+++ DFinal.py
@@ -0,0 +1,52 @@
+import random
+
+def main():
+    infile = open("dictionary.txt", "r")
+    word_list = infile.read().splitlines()
+    
+    all_five = open("Wordle.txt", "r")
+    all_fivewords = all_five.read().splitlines()
+    
+    word_loop = "Yes"
+    while word_loop == "Yes":
+    
+        print("DIRECTIONS:")
+        print("? is like gray in wordle, it means that there is no letter from the word there")
+        print("# is like yellow in wordle, it means that the letter is in the word, just not in the right spot")
+        print("I will print the letter if it is in the word and in the right spot")
+
+        count = 1
+        randomint = random.randint(0,8938)
+        scramdomword = all_fivewords[randomint]
+        
+        for i in range(6):
+            print()
+            my_guess = input("Please enter a guess for a five letter word in all caps: ")
+            for x in range(5):
+                if scramdomword[x] == my_guess[x] and my_guess[x] in scramdomword:
+                    print(my_guess[x], end = '')
+                elif my_guess[x] in scramdomword:
+                    print("#", end = '')
+                elif scramdomword[x] != my_guess[x] and my_guess[x] not in scramdomword:
+                    print("?", end = '')
+                    
+
+            print()
+            if my_guess == scramdomword:
+                print("Congrats you got it in", count, "guesses!")
+                print("The word was", scramdomword)
+                break
+            count = count + 1
+            
+            if count == 6 and scramdomword != my_guess:
+                print("Sorry, you did not get the word in six guesses. The word was", scramdomword)
+                break
+       
+
+               
+        word_loop = input("Do you want to play again? ")
+        
+        infile.close()
+        all_five.close()
+    
+main()
